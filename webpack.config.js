c3a2ad46757133bacfc4c9a35dbccfa1ad06fc9c
Nicolas Olaya <111377816+NicolasAndrehh@users.noreply.github.com>
2023-03-14 09:37:16 -0500

    Fix image issue

--- webpack.config.js
+++ webpack.config.js
@@ -32,18 +32,6 @@ module.exports = {
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[path][name].[ext]',
-              outputPath: 'images/',
-            },
-          },
-        ],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
         generator: {
           filename: 'img/[name][ext]',
