be5c8b0e9a2465298a6231b311330cc45110ce23
johnie261 <njorogejohn261@gmail.com>
2023-03-15 16:54:48 +0300

    Add comment section

--- src/popup/modal.js
+++ src/popup/modal.js
@@ -30,6 +30,23 @@ const displayModal = (id) => {
             <div class="box"> Type : ${character.type} </div>
             <div class="box"> Gender : ${character.gender} </div>
         </div>
+        <div class="comment-box">
+          <h2 class="title"> Comments (2) </h2>
+          <div class="comment-container">
+            <p> 15/03/2023 John: I'd love to buy it so much </p>
+            <p> 15/03/2023 John: I'd love to buy it </p>
+          </div>
+        </div>
+        <form action="#" class="inputComments">
+          <h2>Add a comment</h2>
+          <div class="inputBox">
+            <input class="name" type="text" placeholder="Your name" required>
+            <textarea class="text" id="message" name="message" rows="8" cols="37" placeholder="Your insights" required></textarea>
+            <div class="submit-btn">
+              <button class="submit">Comment</button>
+            </div>
+          </div>
+        </form>      
     `;
 
           modalContainer.innerHTML = html;
