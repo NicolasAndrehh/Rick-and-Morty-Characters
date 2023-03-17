4dc65f5e55dff484bf41f45e7a25b956645857b0
Nicolas Olaya <111377816+NicolasAndrehh@users.noreply.github.com>
2023-03-16 20:16:49 -0500

    Add comment

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './style.scss';
 import './popup/modal.js';
 import { displayLogo, displayCharacters } from './modules/pageRenderer.js';
 
+// Display content
 document.addEventListener('DOMContentLoaded', () => {
   displayLogo();
   displayCharacters();
